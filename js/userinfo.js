f84231c50cacba187341f469ff82dd7012f9c0f4
achenjs <hellowwc@aliyun.com>
2019-03-10 16:22:21 +0800

    修复车牌号bug

--- js/userinfo.js
+++ js/userinfo.js
@@ -12,6 +12,137 @@
 //     'minDate': (new Date().getFullYear()-3) + '-' + 1 + '-' + 1, //最小日期
 //     'maxDate': (new Date().getFullYear()+3) + '-' + 12 + '-' + 31 //最大日期
 // });
+var provinces = new Array("京","沪","浙","苏","粤","鲁","晋","冀",
+    "豫","川","渝","辽","吉","黑","皖","鄂",
+    "津","贵","云","桂","琼","青","新","藏",
+    "蒙","宁","甘","陕","闽","赣","湘");
+
+var keyNums = new Array("0","1","2","3","4","5","6","7","8","9",
+    "Q","W","E","R","T","Y","U","I","O","P",
+    "A","S","D","F","G","H","J","K","L",
+    "OK","Z","X","C","V","B","N","M","Del");
+var next=0;
+function showProvince(){
+    $("#pro").html("");
+    var ss="";
+    for(var i=0;i<provinces.length;i++){
+        ss=ss+addKeyProvince(i)
+    }
+    $("#pro").html("<ul class='clearfix ul_pro'>"+ss+"<li class='li_close' onclick='closePro();'><span>关闭</span></li><li class='li_clean' onclick='cleanPro();'><span>清空</span></li></ul>");
+}
+function showKeybord(){
+    $("#pro").html("");
+    var sss="";
+    for(var i=0;i<keyNums.length;i++){
+        sss=sss+'<li class="ikey ikey'+i+' '+(i>9?"li_zm":"li_num")+' '+(i>28?"li_w":"")+'" ><span onclick="choosekey(this,'+i+');">'+keyNums[i]+'</span></li>'
+    }
+    $("#pro").html("<ul class='clearfix ul_keybord'>"+sss+"</ul>");
+}
+function addKeyProvince(provinceIds){
+    var addHtml = '<li>';
+    addHtml += '<span onclick="chooseProvince(this);">'+provinces[provinceIds]+'</span>';
+    addHtml += '</li>';
+    return addHtml;
+}
+
+function chooseProvince(obj){
+    $(".input_pro span").text($(obj).text());
+    $(".input_pro").addClass("hasPro");
+    $(".input_pp").find("span").text("");
+    $(".ppHas").removeClass("ppHas");
+    next=0;
+    showKeybord();
+}
+
+
+function choosekey(obj,jj){
+    if(jj==29){
+        alert("车牌："+$(".car_input").attr("data-pai"));
+        layer.closeAll();
+    }else if(jj==37){
+        if($(".ppHas").length==0){
+            $(".hasPro").find("span").text("");
+            $(".hasPro").removeClass("hasPro");
+            showProvince();
+            next=0;
+        }
+        $(".ppHas:last").find("span").text("");
+        $(".ppHas:last").removeClass("ppHas");
+        next=next-1;
+        if(next<1){
+            next=0;
+        }
+        console.log(next);
+    }else{
+        if(next>5){
+            return
+        }
+        console.log(next);
+        for(var i = 0; i<$(".input_pp").length;i++){
+            if(next==0 & jj<10 & $(".input_pp:eq("+next+")").hasClass("input_zim")){
+                layer.open({
+                    content: '车牌第二位为字母',
+                    skin: 'msg',
+                    time: 1
+                });
+                return
+            }
+            $(".input_pp:eq("+next+")").find("span").text($(obj).text());
+            $(".input_pp:eq("+next+")").addClass("ppHas");
+            next=next+1;
+            if(next>5){
+                next=6;
+            }
+            getpai();
+            return
+        }
+
+    }
+
+}
+function closePro(){
+    layer.closeAll()
+}
+function cleanPro(){
+    $(".ul_input").find("span").text("");
+    $(".hasPro").removeClass("hasPro");
+    $(".ppHas").removeClass("ppHas");
+    next=0;
+}
+function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+function getpai(){
+    var pai=trimStr($(".car_input").text());
+    $(".car_input").attr("data-pai",pai);
+}
+window.onload = function() {
+
+    $(".input_pro").click(function(){
+        layer.open({
+            type: 1
+            ,content: '<div id="pro"></div>'
+            ,anim: 'up'
+            ,shade :false
+            ,style: 'position:fixed; bottom:0; left:0; width: 100%; height: auto; padding:0; border:none;'
+        });
+        showProvince()
+    })
+    $(".input_pp").click(function(){
+        if($(".input_pro").hasClass("hasPro")){ // 如果已选择省份
+            layer.open({
+                type: 1
+                ,content: '<div id="pro"></div>'
+                ,anim: 'up'
+                ,shade :false
+                ,style: 'position:fixed; bottom:0; left:0; width: 100%; height: auto; padding:0; border:none;'
+            });
+            showKeybord()
+        }else{
+            $(".input_pro").click()
+        }
+    })
+
+
+}
 $(function() {
 
 
@@ -371,7 +502,7 @@ function sureJump(){
         name:  $('#applicant').val(),           // 姓名
         phoneNum: phone,         // 手机号
         cardNum: $('#idCard').val(),        // 身份证号
-        licenseNum: $('#licenseNum').html(),     // 车牌号
+        licenseNum: $(".car_input").attr("data-pai"),     // 车牌号
         companyCode: $('#company').val(),    // 所属公司
         engineNum : $('#chejiaCode').val(),     // 车架号
         photoList: [],
@@ -416,6 +547,7 @@ function submit(){
 
 }
 function verfilFrom() {
+    console.log($(".car_input").attr("data-pai"),next)
     var userinfo = {
         name: '',           // 姓名
         mobile: '',         // 手机号
@@ -451,9 +583,8 @@ function verfilFrom() {
         tanwin('请选择所属公司');
         return false;
     }
-    console.log(licenseNum)
     //  验证车牌号
-    if (!(licenseNum)) {
+    if (!($(".car_input").attr("data-pai")) && next !=6) {
         tanwin('车牌号填写有误');
         return false;
     }
